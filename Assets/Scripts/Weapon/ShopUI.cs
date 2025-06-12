d4581eab0a51b24f5e4b8cd7439a9a23242e3baa
donggeunchoi <cdg01063149045@gmail.com>
2025-06-12 14:39:46 +0900

    chore  update
    무기 이미지 업로드

---
+++ Assets/Scripts/Weapon/ShopUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopUI : MonoBehaviour
+{
+    [Header("Buttons")] 
+    public Button equipButton;
+    public Button nextButton;
+    public Button undoBuoon;
+    public Button upgradeButton;
+    public Button buyButton;
+    public Button backButton;
+    
+}
