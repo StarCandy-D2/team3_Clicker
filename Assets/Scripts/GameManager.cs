f788caa2db10be52cf1b6c23486dd9501b2d3998
StarCandy-D2 <kdh04090@gmail.com>
2025-06-12 12:02:09 +0900

    feat GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public UserData currentUser;
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+}
