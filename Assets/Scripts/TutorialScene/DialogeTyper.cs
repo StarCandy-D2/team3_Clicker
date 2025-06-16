2ca0315ee00ebfa4de20e9eceb23a709905a0dc1
hbks0224 <hbks1234@gmail.com>
2025-06-16 17:53:40 +0900

    feat 인트로 추가

--- Assets/Scripts/TutorialScene/DialogeTyper.cs
+++ Assets/Scripts/TutorialScene/DialogeTyper.cs
@@ -1,80 +1,38 @@
 ﻿using System.Collections;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class DialogueTyper : MonoBehaviour
 {
     public TMP_Text dialogueText;
+    public float typingSpeed = 0.01f;
     public AudioSource audioSource;
     public AudioClip typeSFX;
     public float typingSpeed = 0.04f;
+    public bool IsTyping { get; private set; }
 
-    private Coroutine typingCoroutine;
-    private string currentMessage;
-    private bool canClickToProceed = false;
-
-    public UnityEvent onDialogueComplete;
-    private void Awake()
-    {
-        StartCoroutine(EnableClickDelay(0.1f)); // 실행 여부만 테스트용
-    }
     public void StartDialogue(string message)
     {
-
-        if (typingCoroutine != null)
-            StopCoroutine(typingCoroutine);
-
-        currentMessage = message;
-        typingCoroutine = StartCoroutine(TypeText(message));
-
-        canClickToProceed = false;
-        StartCoroutine(EnableClickDelay(0.1f));
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(message));
     }
 
-    IEnumerator EnableClickDelay(float delay)
+    IEnumerator TypeSentence(string sentence)
     {
-        yield return new WaitForSeconds(delay);
-        canClickToProceed = true;
-    }
-
-    IEnumerator TypeText(string message)
-    {
+        IsTyping = true;
         dialogueText.text = "";
 
-        for (int i = 0; i < message.Length; i++)
+        foreach (char letter in sentence)
         {
-            dialogueText.text += message[i];
-
-            if (typeSFX && audioSource)
+            dialogueText.text += letter;
+            if (letter != ' ' &&typeSFX && audioSource)
+            {
                 audioSource.PlayOneShot(typeSFX, 0.5f);
 
+            }
             yield return new WaitForSeconds(typingSpeed);
         }
 
-        typingCoroutine = null; // 타이핑 끝남
-    }
-    private void Update()
-    {
-
-        if (!canClickToProceed)
-        {
-            return;
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-
-            if (typingCoroutine != null)
-            {
-                StopCoroutine(typingCoroutine);
-                dialogueText.text = currentMessage;
-                typingCoroutine = null;
-            }
-            else
-            {
-                onDialogueComplete?.Invoke();
-            }
-        }
+        IsTyping = false;
     }
 }
